c7e57f424a76b00a202ea6b1af65574062b0edc9
drewenia <egedata@gmail.com>
2021-02-27 19:57:30 +0300

    CategoryList icerisinde basit olarak bir state tanimlamasi yapildi ve component'in icerisinde bir ListGroup icerisinde data kullanildi. Herhangi bir dosyadan yada DB'den dosyayi cekmek yerine hard code olarak state objesinin icerisini doldurduk. Ileride bir JSON dosyasindan bu category'leri cekecegiz

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import {ListGroup, ListGroupItem} from "reactstrap";
 
 class CategoryList extends Component {
+    state = {
+        categories: [
+            {categoryId: "1", categoryName: "Beverages"},
+            {categoryId: "2", categoryName: "Condiment"},
+            {categoryId: "3", categoryName: "Drinks"}
+        ]
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.info.title}</h1>
+                <ListGroup>
+                    {this.state.categories.map(cat=>
+                        <ListGroupItem key={cat.categoryId}>{cat.categoryName}</ListGroupItem>
+                    )}
+                </ListGroup>
             </div>
         );
     }
