4ddab50be117739e72874795cdcfbb6ad9689e3a
drewenia <egedata@gmail.com>
2021-02-28 18:12:40 +0300

    React projesini adim adim kaydettigim react.docx dosyasini stage area'ya kaydettim.
    CategoryList componentinde categoryId olarak bulunan alanlari id ismi
    ile degistirdim
    
    category'lere clicklendiginde bir h4 etiketine tiklanan category'nin
    ismini basan onClick eventini tanimladim.
    
    CategoryList componentini refactor ettim.

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -4,26 +4,30 @@ import {ListGroup, ListGroupItem} from "reactstrap";
 class CategoryList extends Component {
     state = {
         categories: [
-            {categoryId: "1", categoryName: "Beverages"},
-            {categoryId: "2", categoryName: "Condiment"},
-            {categoryId: "3", categoryName: "Drinks"}
+            {id: "1", categoryName: "Beverages"},
+            {id: "2", categoryName: "Condiment"},
+            {id: "3", categoryName: "Drinks"}
         ],
-        currentCategory:""
+        currentCategory: ""
     }
 
-    changeCurrentCategoryAndSetTextH4Tag = (cat)=>{
-        this.setState({currentCategory:cat.categoryName})
+    changeCurrentCategoryAndSetTextH4Tag = (cat) => {
+        this.setState({currentCategory: cat.categoryName})
     }
 
     render() {
         return (
             <div>
                 <h1>{this.props.info.title}</h1>
-                <ListGroup>
-                    {this.state.categories.map(cat=>
-                        <ListGroupItem key={cat.categoryId}>{cat.categoryName}</ListGroupItem>
-                    )}
-                </ListGroup>
+               <ListGroup>
+                   {this.state.categories.map(cat=>
+                       <ListGroupItem
+                           onClick={()=>this.changeCurrentCategoryAndSetTextH4Tag(cat)}
+                           key={cat.id}>{cat.categoryName}
+                       </ListGroupItem>
+                   )}
+               </ListGroup>
+                <h4>{this.state.currentCategory}</h4>
             </div>
         );
     }
