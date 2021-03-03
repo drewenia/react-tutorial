843006f06cbdabbfab0e156e3bf26274839f27a6
drewenia <egedata@gmail.com>
2021-03-03 16:38:13 +0300

    Ana component'de App.js icerisinde;
    state olarak currentCategory
    
    setCurrentCategory adinda methodlar eklendi
    
    Component'ler arasi iletisim tek yonlu oldugu icin state ve method ana
    componente tasinmis oldu

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import {Component} from "react/cjs/react.production.min";
 
-class App extends Component{
-    categoryInfo = {title:"Category List"}
-    productInfo = {title:"Product List"}
-    render(){
-        return(
+class App extends Component {
+    categoryInfo = {title: "Category List"}
+    productInfo = {title: "Product List"}
+
+    state = {
+        currentCategory: ""
+    }
+
+    setCurrentCategory = (cat) => {
+        this.setState({currentCategory: cat.categoryName})
+    }
+
+    render() {
+        return (
             <div className="App">
                 <Container>
-                    <Row><Navi/></Row>
                     <Row>
-                        <Col xs={3}><CategoryList info={this.categoryInfo}/></Col>
-                        <Col xs={9}><ProductList info={this.productInfo}/></Col>
+                        <Navi/>
+                    </Row>
+                    <Row>
+                        <Col xs={3}><CategoryList
+                            info={this.categoryInfo}
+                            currCategory={this.state.currentCategory}
+                            setCategory={this.setCurrentCategory}
+                        />
+                        </Col>
+                        <Col xs={9}><ProductList
+                            info={this.productInfo}/></Col>
                     </Row>
                 </Container>
             </div>
