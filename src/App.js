d9ec32be187be3dd3b824e5d5f2eede6f4258552
drewenia <egedata@gmail.com>
2021-03-03 15:46:16 +0300

    App.js ana componenti function olmaktan cikarilip Class sekline cevirildi.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import {Col, Container, Row} from "reactstrap";
 import Navi from './Navi'
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
+import {Component} from "react/cjs/react.production.min";
 
-function App() {
-    let categoryInfo = {title:"this is category title"};
-    let productInfo = {title:"this is product title"};
-    return (
-        <div className="App">
-            <Container>
-                <Row><Navi/></Row>
-                <Row>
-                    <Col xs={3}><CategoryList info={categoryInfo}/></Col>
-                    <Col xs={9}><ProductList info={productInfo}/></Col>
-                </Row>
-            </Container>
-        </div>
-    );
+class App extends Component{
+    categoryInfo = {title:"Category List"}
+    productInfo = {title:"Product List"}
+    render(){
+        return(
+            <div className="App">
+                <Container>
+                    <Row><Navi/></Row>
+                    <Row>
+                        <Col xs={3}><CategoryList info={this.categoryInfo}/></Col>
+                        <Col xs={9}><ProductList info={this.productInfo}/></Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default App;
+
