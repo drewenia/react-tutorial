dfd00c660f041c081fd9662a8a0408dd0bf037e7
drewenia <egedata@gmail.com>
2021-03-05 15:11:50 +0300

    ProductList componentinde bir table icerisinde db.json dosyasindaki Product'lar listelendi

--- src/ProductList.js
+++ src/ProductList.js
@@ -5,6 +5,30 @@ class ProductList extends Component {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Quantity Per Unit</th>
+                        <th>Unit Price</th>
+                        <th>Units In Stock</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.props.products.map(product =>
+                            <tr key={product.id}>
+                                <th scope="row">{product.id}</th>
+                                <td>{product.productName}</td>
+                                <td>{product.quantityPerUnit}</td>
+                                <td>{product.unitPrice}</td>
+                                <td>{product.unitsInStock}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
             </div>
         );
     }
